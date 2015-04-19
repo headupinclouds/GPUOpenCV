f994f8995af2d57c2e1eeaf7edbfec4bcfeb23e9
David Hirvonen <dhirvonen@elucideye.com>
2015-04-19 18:49:55 -0400

    Rewrote the GPUImage sample app CMakeLists.txt using the ios sample app provided in ruslo/sugar

---
+++ src/app/SimpleVideoFilter/SimpleVideoFilter/sugar.cmake
@@ -0,0 +1,21 @@
+# Copyright (c) 2013, Ruslan Baratov
+# All rights reserved.
+
+if(DEFINED SOURCES_SUGAR_CMAKE)
+  return()
+else()
+  set(SOURCES_SUGAR_CMAKE 1)
+endif()
+
+include(sugar_files)
+
+sugar_files(
+    SOURCES
+    main.m
+    SimpleVideoFilterAppDelegate.h
+    SimpleVideoFilterAppDelegate.mm
+    SimpleVideoFilterViewController.h
+    SimpleVideoFilterViewController.mm
+    NativeVideoFrameHandler.h
+    NativeVideoFrameHandler.mm
+)
