5b5a9e9f3849924568295015538c1aa09d859b0a
David Hirvonen <dhirvonen@elucideye.com>
2015-06-23 15:49:29 -0700

    Removed spurious project() commands; added CMake package CONFIG lib installation syntax

--- bin/build-ios.sh
+++ bin/build-ios.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-build.py --toolchain ios-8-2-arm64 --verbose --fwd HUNTER_CONFIGURATION_TYPES=Release --jobs 8 --open
+build.py --toolchain ios-8-2-arm64 --verbose --fwd HUNTER_CONFIGURATION_TYPES=Release \
+BUILD_EXAMPLES=OFF \
+--config Release \
+--jobs 8 --framework --open
 
 
