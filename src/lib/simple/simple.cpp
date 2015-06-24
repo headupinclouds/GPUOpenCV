aec303b2fae859c254449b3342e493693971e03d
David Hirvonen <dhirvonen@elucideye.com>
2015-06-24 10:38:00 -0700

    Add dependency on boost::filesystem in libsimple to test static lib dependencies

--- src/lib/simple/simple.cpp
+++ src/lib/simple/simple.cpp
@@ -1,11 +1,20 @@
 #include "simple/simple.hpp"
 #include "simple/simple_private.hpp"
 
+#if TEST_SCOPE_ISSUE
+  // Now test a LIB::LIB library
+  #include <boost/filesystem.hpp>
+  #include <iostream> // std::cout
+#endif
+
 _BEGIN_SIMPLE
 
 GPUOpenCVSimple::GPUOpenCVSimple()
 {
-    
+#if TEST_SCOPE_ISSUE
+  namespace fs = boost::filesystem;
+  std::cout << "Current path: " << fs::current_path() << std::endl;    
+#endif
 }
 
 int GPUOpenCVSimple::operator()(int x, int y) 
