0107d7bd22b538367e183614b6419a5ddfcf70ca
David Hirvonen <dhirvonen@elucideye.com>
2015-06-23 17:49:24 -0700

    Added opencv dependency to the internal simple test lib

--- src/lib/complex/complex.cpp
+++ src/lib/complex/complex.cpp
@@ -2,6 +2,7 @@
 
 // Use OpenCV functionality to exercise 3rd party static dependency
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 _BEGIN_COMPLEX
 
@@ -15,6 +16,9 @@ int GPUOpenCVComplex::operator()(int x, int y)
 #if 0
     return std::sqrt(x*x + y*y);
 #else
+    // Some random opencv usage to test linkage:
+    cv::Mat image(100, 100, CV_8UC1);
+    cv::cvtColor(image, image, cv::COLOR_GRAY2BGR);
     return static_cast<int>(cv::norm(cv::Point(x, y))); // use this just to test dependency
 #endif
 }
